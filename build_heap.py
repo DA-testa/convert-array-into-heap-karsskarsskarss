9c549a9e8af754510e751290e6a14a25c076c6b2
karsskarsskarss <ajkarthurs@gmail.com>
2023-03-13 12:51:32 +0200

    1.2

--- build_heap.py
+++ build_heap.py
@@ -2,9 +2,7 @@
 
 
 def build_heap(data):
-    #swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    
     swaps = []
     n = len(data)
     for i in range (n // 2, -1, -1):
@@ -14,7 +12,7 @@ def build_heap(data):
 def sift_down(i, data, swaps):
     n = len(data)
     min_index = i
-    left = 2 * i + 2
+    left = 2 * i + 1
     if left < n and data[left] < data[min_index]:
         min_index = left
     right = 2 * i + 2
@@ -28,32 +26,12 @@ def sift_down(i, data, swaps):
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
 if __name__ == "__main__":
     main()
