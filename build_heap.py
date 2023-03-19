57f174df4d098aff2cdaf4602b8587b1217103a7
karsskarsskarss <ajkarthurs@gmail.com>
2023-03-19 23:44:35 +0200

    upd5

--- build_heap.py
+++ build_heap.py
@@ -1,40 +1,64 @@
-# python3
+#artūrs jānis karss 211recro029 it 1. grupa
+from heapq import heapify
 
-#test
-def heapify(data, n, i, swaps):
-    smallest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
 
-    if left < n and data [left] < data [smallest]:
-        smallest = left
-
-    if left < n and data [right] < data [smallest]:
-        smallest = right  
-    
-    if smallest != i:
-        swaps.append((i, smallest)) 
-        data[i], data[smallest] = data[smallest], data[i]
-        heapify(data, n, smallest, swaps)
-def build_heap(data):
-    
-    swaps = []
+def heapify(data, i, swaps):
     n = len(data)
-    for i in range (n // 2, -1, -1, -1):
-        heapify(data, n, i, swaps)
+    left_child = 2*i + 1
+    right_child = 2*i + 2
+    minimum = i
+    if left_child < n and data[left_child] < data[minimum]:
+        minimum = left_child
+    if right_child < n and data[right_child] < data[minimum]:
+        minimum = right_child
+    if i != minimum:
+        data[i], data[minimum] = data[minimum], data[i]
+        swaps.append((i, minimum))
+        heapify(data, minimum, swaps)
+
+
+def build_heap(data):
+    n = len(data)
+    swaps = []
+    for i in range(n // 2, -1, -1):
+        heapify(data, i, swaps)
     return swaps
 
 
-
-
-
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
-    assert len(data) == n
+    try:
+        text = input()
+        if text not in ["I", "F"]:
+            print("Invalid input.")
+            return
+        if text == "I":
+            n = int(input())
+            data = list(map(int, input().split()))
+        elif text == "F":
+            filename = input()
+            if not filename.endswith(".txt"):
+                print("Wrong file name.")
+                return
+            with open("test/" + filename, "r") as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+        else:
+            print("Invalid input.")
+            return
+    except:
+        print("Invalid input.")
+        return
+
     swaps = build_heap(data)
-    print(len(swaps))
+
+    if len(swaps) > 4 * len(data):
+        print("Out of bounds.")
+        return
+
+    print(f"Number of swaps: {len(swaps)}")
     for i, j in swaps:
-        print(i, j)
+        print(f"{i} {j}")
+
+
 if __name__ == "__main__":
     main()
